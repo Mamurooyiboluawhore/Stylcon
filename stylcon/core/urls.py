1a92bf2e1179c9060e2068664ea2367d747924a0
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2024-03-17 19:07:58 +0100

    implemented auth

--- stylcon/core/urls.py
+++ stylcon/core/urls.py
@@ -15,8 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, inlucde
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', inlucde('core.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_views(), name='token_refresh'),
+    path('api/v1/auth/', include('accounts.urls'))
 ]
