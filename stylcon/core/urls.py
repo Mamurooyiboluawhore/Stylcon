daa627835292cc23828876d18f4b8c0c1bbf367e
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2024-03-20 23:18:16 +0100

    implemented cataloge

--- stylcon/core/urls.py
+++ stylcon/core/urls.py
@@ -41,6 +41,7 @@ urlpatterns = [
     # path('api/', include('core.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/catalogue/', include('catalogue.urls')),
     path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0),name='schema-swagger-ui'),
     path('api/v1/auth/', include('accounts.urls'))
 ]
