da18554a6bc4b2d9037eab6d2b954dbd889eb2cb
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2024-03-24 10:45:52 +0100

    created models for catalogue

--- stylcon/catalogue/models.py
+++ stylcon/catalogue/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from datetime import timezone
 # Create your models here.
+
+class Catalogue(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+    createdat = models.DateTimeField(db_column='createdAt', default=timezone.now, blank=True, null=True)
+    updatedat = models.DateTimeField(db_column='updatedAt', default=timezone.now, blank=True, null=True)
