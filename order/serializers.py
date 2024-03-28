efabd7d9260ff37e016df8cfdc01b062a497807a
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2024-03-28 16:11:26 +0100

    added orderCreateSerializer

--- order/serializers.py
+++ order/serializers.py
@@ -1,12 +1,27 @@
 from rest_framework import serializers
 from .models import Order
 
-
 class OrderSerializers(serializers.ModelSerializer):
     """
-        Order seializers class;
-        serializes and deserializes data
+    Order serializers class;
+    Serializes and deserializes data
     """
     class Meta:
         model = Order
         fields = ('id', 'customer', 'product', 'quantity')
+
+class OrderCreateSerializer(serializers.ModelSerializer):
+    """
+    Order create serializer class;
+    Used to validate the incoming data before saving it in the database
+    """
+    class Meta:
+        model = Order
+        fields = ('product', 'name', 'created', 'quantity', 'subtotal', 'email', 'address', 'modified', 'postalCode', 'city', 'country', 'user')
+        extra_kwargs = {
+            'id': {'read_only': True},
+            'created': {'read_only': True},
+            'modified': {'read_only': True},
+            'customer': {'read_only': True},  
+            'subtotal': {'required': False},  
+        }
