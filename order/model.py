9d56d692f0200d8e38dde90591eefb1628cf6618
Mamurooyiboluawhore <missmarvelous2016@gmail.com>
2024-04-23 16:29:00 +0100

    set draft as default in models.py

--- order/model.py
+++ order/model.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.conf import settings
 
+
+# Create your models here.
 class Order(models.Model):
+    """
+        Order model for creating order table
+    """
+
     DRAFT = 'draft'
     FINALIZED = 'finalized'
     CANCELLED = 'cancelled'
@@ -11,30 +17,34 @@ class Order(models.Model):
         (CANCELLED, 'Cancelled'),
     ]
 
+
     id = models.UUIDField(primary_key=True)
     email = models.EmailField(max_length=60, unique=True)
     name = models.CharField(max_length=50, null=True)
     address = models.TextField()
     quantity = models.IntegerField()
     city = models.CharField(max_length=100)
-    postal_code = models.CharField(max_length=20)
+    postalCode = models.CharField(max_length=20)
     country = models.CharField(max_length=80)
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     product = models.ForeignKey('catalogue.Catalogue', on_delete=models.CASCADE, blank=True, null=True)
     customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     subtotal = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
-    order_status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=DRAFT)
+    order_status = models.TextField(choices=STATUS_CHOICES, default=DRAFT)
 
     def finalize(self):
-        self.order_status = self.FINALIZED
+        self.status = self.FINALIZED
         self.save()
 
     def cancel(self):
-        self.order_status = self.CANCELLED
+        self.status = self.CANCELLED
         self.save()
 
     class Meta:
+        '''
+            metadata for the Order model
+        '''
         verbose_name = 'Order'
         verbose_name_plural = 'Orders'
         db_table = "order"
